99fc547171421fa1c1c3fb8eb8e3e210c98a0b79
Paulo Willian <paulowos@gmail.com>
2024-03-25 12:34:11 -0300

    feat: Core Creation

---
+++ Core/Account.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core;
+
+public class Account
+{
+    [Key] public int Id { get; set; }
+
+    [Required] [MinLength(3)] public string Name { get; set; }
+
+    [Required] public int BankCode { get; set; }
+
+    [Required] public string BankName { get; set; }
+
+    [Required] [MinLength(11)] public string PixKey { get; set; }
+}
