4456d289f443fbd0a6d27945c627d6fb3993b49c
Paulo Willian <paulowos@gmail.com>
2024-03-25 23:33:33 -0300

    feat: Added PixKey index migration

--- AccountApi/Migrations/20240326021657_Added PixKey Index.Designer.cs
+++ AccountApi/Migrations/20240326021657_Added PixKey Index.Designer.cs
@@ -2,6 +2,7 @@
 using AccountApi.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AccountApi.Migrations
 {
     [DbContext(typeof(AccountContext))]
-    partial class AccountContextModelSnapshot : ModelSnapshot
+    [Migration("20240326021657_Added PixKey Index")]
+    partial class AddedPixKeyIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
