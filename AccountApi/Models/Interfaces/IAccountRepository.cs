da94db89cca2d4dd3c2af22cf0531087479be0f7
Paulo Willian <paulowos@gmail.com>
2024-03-25 23:34:08 -0300

    feat: update Account Repository

--- AccountApi/Models/Interfaces/IAccountRepository.cs
+++ AccountApi/Models/Interfaces/IAccountRepository.cs
@@ -5,5 +5,5 @@ namespace AccountApi.Models.Interfaces;
 public interface IAccountRepository
 {
     public AccountOutDto? GetByPixKey(string pixKey);
-    public AccountOutDto Add(AccountInDto accountDto);
+    public void Add(AccountInDto accountDto);
 }
