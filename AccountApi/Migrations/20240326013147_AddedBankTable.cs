5d830398d1c0481aae53a7b2326ad615ec65c311
Paulo Willian <paulowos@gmail.com>
2024-03-25 22:58:49 -0300

    feat: New Migration

---
+++ AccountApi/Migrations/20240326013147_AddedBankTable.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AccountApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedBankTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BankName",
+                table: "Accounts");
+
+            migrationBuilder.RenameColumn(
+                name: "BankCode",
+                table: "Accounts",
+                newName: "BankId");
+
+            migrationBuilder.CreateTable(
+                name: "Banks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banks", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_BankId",
+                table: "Accounts",
+                column: "BankId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Banks_BankId",
+                table: "Accounts",
+                column: "BankId",
+                principalTable: "Banks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Banks_BankId",
+                table: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Banks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_BankId",
+                table: "Accounts");
+
+            migrationBuilder.RenameColumn(
+                name: "BankId",
+                table: "Accounts",
+                newName: "BankCode");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BankName",
+                table: "Accounts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
